933b42e0fe8e12d8235970a4e618438c653e9956
Eduardo <lourenco.e@pucpr.edu.br>
2025-11-13 19:00:46 -0300

    feat: added sections

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,24 @@ public class Main {
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
 
-        String[] aleatorio_100 = fileReader.readLines("aleatorio_100.csv");
+        section("Aleatório", "aleatorio");
+        section("Crescente", "crescente");
+        section("Decrescente", "decrescente");
+    }
 
-        for (String line : aleatorio_100) {
-            //TODO: fazer algo
-        }
+    public static void section(String name, String baseFileName) {
+        System.out.println("Section " + name);
+        System.out.println("Base file name: " + baseFileName);
+        FileReader fileReader = new FileReader();
+
+        String[] set100 = fileReader.readLines(baseFileName + "_100.csv");
+        String[] set1000 = fileReader.readLines(baseFileName + "_1000.csv");
+        String[] set10000 = fileReader.readLines(baseFileName + "_10000.csv");
+
+        System.out.println(set100.length);
+        System.out.println(set1000.length);
+        System.out.println(set10000.length);
+
+        System.out.println("---");
     }
 }
